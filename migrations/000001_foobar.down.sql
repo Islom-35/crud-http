3d73deede0207c86f47c75ce6d86b32b5a71a7db
islom <ismoilovislom589@gmail.com>
2023-11-24 14:50:38 +0500

    remove table movies

--- migrations/000001_foobar.down.sql
+++ migrations/000001_foobar.down.sql
@@ -1,3 +1,1 @@
-drop table if exists movies;
-
 drop table if exists books;
--- migrations/000001_foobar.up.sql
+++ migrations/000001_foobar.up.sql
@@ -1,8 +1,3 @@
-create table if not exists movies (
-    id bigserial primary key,
-    name varchar(30) not null
-);
-
 CREATE TABLE IF NOT EXISTS books (
     id bigserial PRIMARY KEY,
     title varchar(30),
